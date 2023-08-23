6585fadbef0e7cd4f102684d33b018965284009e
ViktoriaPikh <viktoria.pikh@gmail.com>
2023-08-23 13:05:53 +0300

    extended readme, added requirements.txt

--- db_populator.py
+++ db_populator.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 # Create or connect to a SQLite database
@@ -7,7 +8,7 @@ cursor = db_conn.cursor()
 # Create the Orders table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Orders (
-    id INTEGER PRIMARY KEY,
+    order_id INTEGER PRIMARY KEY,
     created_date TEXT,
     merchant_id INTEGER
 )
@@ -16,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Orders (
 # Create the Transactions table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Transactions (
-    id INTEGER PRIMARY KEY,
+    transaction_id INTEGER PRIMARY KEY,
     created_date TEXT,
     transaction_type TEXT,
     order_id INTEGER,
@@ -26,30 +27,29 @@ CREATE TABLE IF NOT EXISTS Transactions (
 """)
 
 # Sample data for Orders
-orders_data = [
-    ("5/15/2023 17:48", 1),
-    ("5/16/2023 1:06", 2),
-    ("5/16/2023 12:46", 2),
-    ("5/16/2023 22:44", 3)
-]
-
 # Insert sample Orders data
-cursor.executemany("INSERT INTO Orders (created_date, merchant_id) VALUES (?, ?)", orders_data)
+with open('Orders.csv', 'r', newline='') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        cursor.execute("""
+        INSERT INTO Orders (order_id, created_date, merchant_id)
+        VALUES (?, ?, ?)
+        """, 
+        (row['order_id'],row['created_date'],row['merchant_id']))
+
 db_conn.commit()
 
-# Sample data for Transactions
-transactions_data = [
-    ("5/15/2023 17:48", "auth", 1, "success"),
-    ("5/16/2023 1:06", "auth", 2, "success"),
-    ("5/16/2023 12:46", "auth", 3, "success"),
-    ("5/16/2023 22:44", "auth", 4, "success"),
-    ("5/19/2023 18:57", "settle", 1, "fail"),
-    ("5/20/2023 3:50", "void", 2, "success"),
-    ("5/21/2023 7:21", "settle", 3, "success")
-]
 
 # Insert sample Transactions data
-cursor.executemany("INSERT INTO Transactions (created_date, transaction_type, order_id, transaction_status) VALUES (?, ?, ?, ?)", transactions_data)
+with open('Transactions.csv', 'r', newline='') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        cursor.execute("""
+        INSERT INTO Transactions (created_date,order_id,transaction_type,transaction_status)
+        VALUES (?, ?, ?, ?)
+        """, 
+        (row['created_date'],row['order_id'],row['transaction_type'],row['transaction_status']))
+
 db_conn.commit()
 
 # Close the database connection
